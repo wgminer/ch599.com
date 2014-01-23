60a2bb2fff0c3bd47bd88cd4e6384fa11fc0ecfe
Will Miner <wgminer@Wills-Mac-Pro.local>
2014-01-22 20:49:26 -0800

    Setting up Angular and Rails PoC

---
+++ client/app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('ch599App', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
