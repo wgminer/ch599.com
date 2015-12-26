0780e1a2cff748ca3c6624f02fc9ee3b78941b80
William Miner <wminer@2u.com>
2015-12-26 09:30:18 -0500

    adding GA tracking

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -5,6 +5,17 @@
 
     <script src="<?php echo base_url() ?>public/js/599.js"></script>
 
+    <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-35251422-1']);
+        _gaq.push(['_trackPageview']);
+        (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
+
     <?php if (isset($user)) : ?>
 
     <script>
