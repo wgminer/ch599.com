94d63102b02594669fc1a2c0fff7eb53b2670c5a
Will Miner <wgminer@gmail.com>
2017-03-05 17:44:11 -0500

    2017 updates

--- application/config/config.php
+++ application/config/config.php
@@ -358,10 +358,11 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 2592000; // 30 Days
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = sys_get_temp_dir();
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
+
 
 /*
 |--------------------------------------------------------------------------
